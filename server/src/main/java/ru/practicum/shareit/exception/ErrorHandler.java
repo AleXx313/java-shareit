6e0db0eac9f3af32fa9309332d2a60f596fec881
Alexey Mironov <alexeymironov1991@gmail.com>
2023-08-19 12:06:54 +0300

    style: v1.2.2 Поправить импорты в server.

--- server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -1,11 +1,9 @@
 package ru.practicum.shareit.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class ErrorHandler {
@@ -21,18 +19,6 @@ public class ErrorHandler {
     public ErrorResponse handleInvalidBookingException(final InvalidBookingException e) {
         return new ErrorResponse(e.getMessage(), e.getMessage());
     }
-
-//    @ExceptionHandler({MethodArgumentNotValidException.class})
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public ErrorResponse handleMethodArgumentNotValidExceptions(final MethodArgumentNotValidException e) {
-//        return new ErrorResponse("Передан некорректный объект!", e.getMessage());
-//    }
-
-//    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public ErrorResponse handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
-//        return new ErrorResponse("Unknown state: " + e.getValue().toString(), e.getMessage());
-//    }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
